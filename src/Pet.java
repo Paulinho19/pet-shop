6ae76b59ca706e35562150880e91d36cfaec8842
Paulo <paulohsantosrocha@gmail.com>
2025-08-30 13:42:15 -0300

    feat: add pet isClean attribute

--- src/Pet.java
+++ src/Pet.java
@@ -1,6 +1,7 @@
 public class Pet {
     private String name;
     private String race;
+    private boolean isClean;
 
     public String getName(){
         return this.name;
@@ -10,12 +11,20 @@ public class Pet {
         return this.race;
     }
 
+    public boolean getIsClean(){
+        return this.isClean;
+    }
+
     public void setName(String name){
         this.name = name;
     }
 
     public void setRace(String race){
        this.race = race;
+    }
+
+    public void setClean(boolean isClean){
+        this.isClean = isClean;
     }
 
 
