da80320bd02fbb7e917ab44797aee0f9b68e3e7e
Paulo <paulohsantosrocha@gmail.com>
2025-08-30 13:33:56 -0300

    feat: add pet class

---
+++ src/Pet.java
@@ -0,0 +1,22 @@
+public class Pet {
+    private String name;
+    private String race;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getRace(){
+        return this.race;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setRace(String race){
+       this.race = race;
+    }
+
+
+}
